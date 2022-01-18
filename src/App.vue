15b9074ddb98adbb47c38570dff3ecca81b207a4
Felix RONDEAU <10324075-xil-f-dev@users.noreply.gitlab.com>
2022-01-18 18:51:35 +0100

    Minor issue

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,8 @@ body {
 @media screen and (min-width: 800px) {
   .section_container {
     padding: 350px 100px 0;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.2) 250px, #121212 350px),
+      url("./assets/background_vegas.jpg");
   }
 }
 
